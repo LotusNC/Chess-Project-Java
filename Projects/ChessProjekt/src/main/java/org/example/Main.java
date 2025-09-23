5b45483a38e40b03f868b8eb88064329016869a2
LotusNC <98554561+LotusNC@users.noreply.github.com>
2025-09-23 07:46:05 +0200

    Main Body Pushed

---
+++ Projects/ChessProjekt/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+            // import type of display or playing session -> board / position
+            // ChessGame game = new ChessGame();
+        }
+    }
+}
